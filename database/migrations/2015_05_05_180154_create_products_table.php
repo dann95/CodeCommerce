8e17ec59b5d0802234dbecbb4367891265203f06
Daniel Rubin <danielrubin95@hotmail.com>
2015-05-15 17:35:24 -0300

    juntando a migration de products

--- database/migrations/2015_05_05_180154_create_products_table.php
+++ database/migrations/2015_05_05_180154_create_products_table.php
@@ -12,9 +12,16 @@ class CreateProductsTable extends Migration {
 	 */
 	public function up()
 	{
+        /**
+         * WESLEY SE VOCÊ OLHAR NO COMMIT ANTERIOR, ESSES CAMPOS JA EXISRTIAM, SÓ ESTAVAM EM OUTRA MIGRATION CHAMADA PRODUCTS,
+         * Q MODIFICAVA A TABELA products.
+         */
 		Schema::create('products', function(Blueprint $table)
 		{
-			$table->increments('id');
+            $table->increments('id');
+            $table->integer('category_id')->unsigned()->default(1);
+            $table->boolean('featured');
+            $table->boolean('recommend');
             $table->string('name',80);
             $table->text('description');
             $table->decimal('price');
