2a89811068ddba7b6888454dcff744c3d3feed8d
Daniel Rubin <danielrubin95@hotmail.com>
2015-05-18 16:12:47 -0300

    hasMany Category-Product

--- app/Category.php
+++ app/Category.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['name' , 'description'];
+
+    public function products()
+    {
+        return $this->hasMany('CodeCommerce\Product');
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name' , 'description' , 'price' , 'category_id' , 'featured' , 'recommend'];
+
+    public function category()
+    {
+        return $this->belongsTo('CodeCommerce\Category');
+    }
 }
