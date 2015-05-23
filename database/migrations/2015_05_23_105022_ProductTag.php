c44850d7de7d33f06766722be2dcbcf69fc7d838
Daniel Rubin <danielrubin95@hotmail.com>
2015-05-23 08:05:45 -0300

    rename pivot table

---
+++ database/migrations/2015_05_23_105022_ProductTag.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProductTag extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('product_tag', function(Blueprint $table)
+		{
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('tag_id')->references('id')->on('tags');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('product_tag');
+	}
+
+}
