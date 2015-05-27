c30a80fe2b555d1d7a1b2758f5b895e1cd79bcba
Daniel Rubin <danielrubin95@hotmail.com>
2015-05-27 16:07:13 -0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,31 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+
+
+    mix.styles([
+        'bootstrap.min.css',
+        'font-awesome.min.css',
+        'prettyPhoto.css',
+        'price-range.css',
+        'responsive.css',
+        'main.css'
+    ] , 'public/css/CodeCommerce.css');
+
+    mix.scripts([
+        'bootstrap.min.js',
+        'contact.js',
+        'gmaps.js',
+        'html5shiv.js',
+        'Jquery.js',
+        'Jquery.prettyPhoto.js',
+        'Jquery.scrollUp.min.js',
+        'main.js',
+        'price-range.js'
+    ] , 'public/js/CodeCommerce.js');
+
+    mix.version(['css/CodeCommerce.css' , 'js/CodeCommerce.js']);
+
+    mix.copy('resources/assets/fonts' , 'public/build/fonts');
+
 });
